a09cd93eb2b47ff786ef4193d6e2b228a477fad0
lyxell <alyxell@kth.se>
2021-06-20 20:23:49 +0200

    Add LICENSE file

--- nway.h
+++ nway.h
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2021, Anton Lyxell
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
